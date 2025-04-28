b1b6655d22273d5875fba11ff51608cfb4af7046
athrocks <atharva0709pardeshi@gmail.com>
2025-04-28 22:09:44 +0530

    GraphQL Integration

--- src/main/java/com/graphql/learn/controller/BookController.java
+++ src/main/java/com/graphql/learn/controller/BookController.java
@@ -2,12 +2,14 @@ package com.graphql.learn.controller;
 
 import com.graphql.learn.entities.Book;
 import com.graphql.learn.services.BookService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
 
     private BookService bookService;
@@ -16,19 +18,78 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @PostMapping
-    public Book create(@RequestBody Book book){
+    @MutationMapping("createBook")
+    public Book create(@Argument("book") BookInput bookInput) {
+        Book book = new Book();
+        book.setTitle(bookInput.getTitle());
+        book.setAuthor(bookInput.getAuthor());
+        book.setPrice(bookInput.getPrice());
+        book.setDesc(bookInput.getDesc());
+        book.setPages(bookInput.getPages());
+
         return this.bookService.create(book);
     }
 
-    @GetMapping
-    public List<Book> getAll(){
+    @QueryMapping("allBooks")
+    public List<Book> getAll() {
         return this.bookService.getAll();
     }
 
-    @GetMapping("/{bookId}")
-    public Book getById(@PathVariable int bookId){
+    @QueryMapping("getBook")
+    public Book get(@Argument int bookId) {
         return this.bookService.get(bookId);
     }
 
+}
+
+
+class BookInput {
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
+
+    public BookInput() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 }
